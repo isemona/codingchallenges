5c5490d7b4bc1a08d1a24961e6efb26c0e3f38ea
Semona Igama <mona@Semonas-MacBook-Pro.local>
2019-01-09 22:24:24 -0800

    Adding comments for understanding

--- 1-FirstReverse.py
+++ 1-FirstReverse.py
@@ -1,4 +1,17 @@
 # https://www.coderbyte.com/editor/First%20Reverse:Python#
 
-Difficulty - Easy
+#Difficulty - Easy
+
+def FirstReverse(str): 
+	# no built in reverse functions for strings
+	# using extended slice syntax (begins and ends at 0)
+	# src - https://www.geeksforgeeks.org/reverse-string-python-5-different-ways/
+    return str[::-1] 
+
+	#You can also do:
+	return ''.join(reversed(str))
+    
+    
+# keep this function call here  
+print FirstReverse(raw_input())
 
